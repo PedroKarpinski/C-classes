abaf82c4e2858e033c931722842e78414c7cd749
Pedro Henrique Karpinski Silva <152297871+PedroKarpinski@users.noreply.github.com>
2024-02-15 18:52:55 +0000

    makefile add

--- AED1/semana1/strings/main.c
+++ AED1/semana1/strings/main.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
 #include "string_lib.h"
+
+#include <stdio.h>
 
 int main(void) {
     char* data1 = "abc";
     int len1 = 3;
     char* data2 = "def";
     int len2 = 3;
+
     String* str1 = string_create(data1, len1);
     String* str2 = string_create(data2, len2);
+    printf("String 1: "); string_print(str1);
+    printf("String 2: "); string_print(str2);
+
     string_append(str1, str2);
-    string_print(str1);
+    printf("String 1 + String 2: "); string_print(str1);
+
+    string_reverse(str1);
+    printf("String 1 + String 2 invertida: "); string_print(str1);
+
     string_destroy(str1);
     string_destroy(str2);
     return 0;
